c429769dd7e1d4d06bcebc0a49369a4aea0225d5
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-03-28 12:08:47 +0200

    Create connection to MySQL database

--- phpDir/src/CRUDApp/login.php
+++ phpDir/src/CRUDApp/login.php
@@ -1,7 +1,32 @@
 <?php
 if(isset($_POST['submit'])) {
-    echo "Data received";
+    // echo "Data received";
+    $user = $_POST['username'];
+    $pass = $_POST['password'];
 }
+
+// Validate form inputs
+if($user && $pass) {
+    echo $user . " " . $pass;
+} else {
+    echo "Username and password fields can not be blank. ";
+}
+
+$host = 'db';
+
+// database username
+$dbname = 'loginapp';
+$dbuser = 'root';
+$dbpass = 'lionPass';
+
+// Check MySQL connection satus
+$conn = new mysqli($host, $dbuser, $dbpass); // MySQL interface, connection between PHP and mySQL db
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "Connected to MySQL server succesfully!";
+}
+
 ?>
 
 <form action="login.php" method="post">
