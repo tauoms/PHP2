413668f717de5b24a1a5f26506b2d481dad163ab
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-03-28 12:50:46 +0200

    Create update.php

---
+++ phpDir/src/CRUDApp/update.php
@@ -0,0 +1,10 @@
+<form action="update.php" method="post">
+    <label for="username">Username: </label>
+    <input type="text" name="username">
+    <label for="password">Password: </label>
+    <input type="password" name="password">
+    <select name="" id="">
+        <option value="">1</option>
+    </select>
+    <input type="submit" name="submit" value="Update">
+</form>
