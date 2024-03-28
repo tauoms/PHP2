b858ef67002949ccdc1d3b2a1d68a8a1ae965f1c
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-03-28 13:54:42 +0200

    Add solution for task 7.php

--- phpDir/src/PHP_practice01/7.php
+++ phpDir/src/PHP_practice01/7.php
@@ -27,7 +27,38 @@
 		Step 4 - Connect to Database and read data
 
 */
+
+$host = 'db';
+// Database user name
+$dbname = 'db7';
+$user = 'root';
+//database user password
+$pass = 'lionPass';
+
+// check the MySQL connection status
+$conn = new mysqli($host, $user, $pass, $dbname);
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
 	
+$query = "SELECT * FROM reports"; //Select everything from users table
+
+
+$result = mysqli_query($conn, $query);
+
+if (!$result) {
+    die('Reading db records failed');
+}
+
+// READ rows from db
+while ($row = mysqli_fetch_assoc($result)) { // Fetch arrays from db
+    ?>
+    <pre>
+        <?php print_r($row); ?>
+    </pre>
+    <?php
+}
+?>
 	?>
 
 
