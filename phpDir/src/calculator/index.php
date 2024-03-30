52a205db91a78dbe89ea8b7e940f3fb77d9fbac7
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-03-30 17:05:22 +0200

    Add working code for calculator

--- phpDir/src/calculator/index.php
+++ phpDir/src/calculator/index.php
@@ -8,37 +8,34 @@ class Calculation {
     public $operator;
 
     public function __construct($int1, $int2, $operator) {
-        $this->int1 = (int)$int1;
-        $this->int2 = (int)$int2;
+        $this->int1 = (float)$int1;
+        $this->int2 = (float)$int2;
         $this->operator = $operator;
     }
 
-    public function calculate(int $int1, int $int2, $operator) {
-        $this->int1 = $int1;
-        $this->int2 = $int2;
-        $this->operator = $operator;
+    public function calculate() {
 
-        switch ($operator) {
+        switch ($this->operator) {
             case "addition":
-                return $int1 + $int2;
+                return $this->int1 + $this->int2;
                 break;
             case "subtraction":
-                return $int1 - $int2;
+                return $this->int1 - $this->int2;
                 break;
             case "division":
-                return $int1 / $int2;
+                return $this->int1 / $this->int2;
                 break;
             case "multiplication":
-                return $int1 * $int2;
+                return $this->int1 * $this->int2;
                 break;
         }
     }
 
 }
 
-if ($_POST['submit']) {
-    new Calculation($_POST['int1'], $_POST['int2'], $_POST['operaotr']);
-    $result = calculate();
+if (isset($_POST['submit'])) {
+    $calculation = new Calculation($_POST['value1'], $_POST['value2'], $_POST['operator']);
+    $result = $calculation->calculate();
 }
 
 
@@ -51,26 +48,30 @@ if ($_POST['submit']) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Calculator</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:ital,wght@0,200..900;1,200..900&display=swap" rel="stylesheet">
 </head>
 <body>
     <div class="calculator">
         <h1>Calculator</h1>
-        <form action="index.php" action="post">
+        <form action="index.php" method="post">
             <label for="value1">Value 1: </label>
             <input type="text" name="value1" />
             <select name="operator" id="operator">
                 <option value="addition">+ Add</option>
-                <option value="subtract">- Subtract</option>
+                <option value="subtraction">- Subtract</option>
                 <option value="division">/ Divide</option>
                 <option value="multiplication">* Multiply</option>
             </select>
-            <br>
+            <br><br>
             <label for="value2">Value 2: </label>
             <input type="text" name="value2" />
             
-            <button type="submit" value="=">
+            <input type="submit" name="submit" value=" = ">
         </form>
-        <p>Result: <?= $result ?></p>
+        <p>Result: </p>
+        <h2><?= $result ?></h2>
     </div>
 </body>
 </html>
