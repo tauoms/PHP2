6c6bf135891d4f8bb646b34475264160e5f539e1
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-04-10 10:32:46 +0300

    Add brackets after new DBConnect

--- phpDir/src/api/index.php
+++ phpDir/src/api/index.php
@@ -4,11 +4,11 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
 
-echo "Testing API";
+// echo "Testing API";
 
 // phpinfo();
 
-$objectDB = new DBConnect;
+$objectDB = new DBConnect();
 $connection = $objectDB->connect();
 
 var_dump($connection);
