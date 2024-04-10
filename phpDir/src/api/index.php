3626345b0079eb433a0c61451f53b7e32f28eaab
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-04-10 11:53:47 +0300

    Add switch statement for POST method

--- phpDir/src/api/index.php
+++ phpDir/src/api/index.php
@@ -11,4 +11,35 @@ header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
 $objectDB = new DBConnect();
 $connection = $objectDB->connect();
 
-var_dump($connection);
+// var_dump($connection);
+
+/**
+ * Read entire file into string
+ */
+
+// print_r(file_get_contents('php://input'));
+
+$user = file_get_contents('php://input');
+$method = $_SERVER['REQUEST_METHOD'];
+
+switch ($method) {
+    case "POST":
+        $user = json_decode(file_get_contents('php://input'));
+        $sql = "INSERT INTO users(id, name, email, mobile, created_at, updated_at) VALUES(null, :name, :email, :mobile, :created_at, :updated_at)";
+        $stmt = $connection->prepare($sql);
+        $created_at = date('Y-m-d');
+        $updated_at = date('Y-m-d');
+        $stmt->bindParam(':name', $user->name);
+        $stmt->bindParam(':email', $user->email);
+        $stmt->bindParam(':mobile', $user->mobile);
+        $stmt->bindParam(':created_at', $created_at);
+        $stmt->bindParam(':updated_at', $updated_at);
+
+        if ($stmt->execute()) {
+            $response = ['status' => 1, 'message' => 'Record created successfully'];
+        } else {
+            $response = ['status' => 0, 'message' => 'Failed to create record'];
+        }
+        echo json_encode($response);
+        break;
+}
